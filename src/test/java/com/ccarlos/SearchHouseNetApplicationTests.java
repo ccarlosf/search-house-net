9be6e47463aae8b8bc4dc8644f304e270c666614
ccarlos <2238513100@qq.com>
2020-04-04 12:48:38 +0800

    3-2 后端框架搭建 (SpringBoot与JPA集成)

---
+++ src/test/java/com/ccarlos/SearchHouseNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SearchHouseNetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
