4eb38009f9688c6d0562fce3cfece4ed353ed247
ccarlos <2238513100@qq.com>
2020-04-04 11:23:22 +0800

    ElasticSearch 搜房网项目

---
+++ src/test/java/com/ccarlos/searchhousenet/SearchHouseNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.searchhousenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchHouseNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
