7717b23d10488dc631c0392853708b42e9e84212
ccarlos <2238513100@qq.com>
2020-04-04 14:37:36 +0800

    4-3 API结构设计_标准制定

--- src/main/java/com/ccarlos/web/controller/HomeController.java
+++ src/main/java/com/ccarlos/web/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.ccarlos.web.controller;
 
+import com.ccarlos.base.ApiResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -11,5 +13,11 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("name","Hello World");
         return "index";
+    }
+
+    @GetMapping(value = "/get")
+    @ResponseBody
+    public ApiResponse get(Model model) {
+        return ApiResponse.ofMessage(200,"成功了");
     }
 }
