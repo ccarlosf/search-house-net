839dfb32278e5566c9973159757305ac38072571
ccarlos <2238513100@qq.com>
2020-04-04 15:10:16 +0800

    4-5 功能性页面开发  403 404 500页面

--- src/main/java/com/ccarlos/web/controller/HomeController.java
+++ src/main/java/com/ccarlos/web/controller/HomeController.java
@@ -20,4 +20,24 @@ public class HomeController {
     public ApiResponse get(Model model) {
         return ApiResponse.ofMessage(200,"成功了");
     }
+
+    @GetMapping("/404")
+    public String notFoundPage() {
+        return "404";
+    }
+
+    @GetMapping("/403")
+    public String accessError() {
+        return "403";
+    }
+
+    @GetMapping("/500")
+    public String internalError() {
+        return "500";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "logout";
+    }
 }
