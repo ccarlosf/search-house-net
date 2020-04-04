67edd7c25100d47fb684e60bccbf63f793f247ef
ccarlos <2238513100@qq.com>
2020-04-04 15:26:13 +0800

    5-1 业务与功能分析设计

---
+++ src/main/java/com/ccarlos/web/controller/admin/AdminController.java
@@ -0,0 +1,47 @@
+package com.ccarlos.web.controller.admin;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+@Controller
+public class AdminController {
+
+
+    /**
+     * 后台管理中心
+     * @return
+     */
+    @GetMapping("/admin/center")
+    public String adminCenterPage() {
+        return "admin/center";
+    }
+
+    /**
+     * 欢迎页
+     * @return
+     */
+    @GetMapping("/admin/welcome")
+    public String welcomePage() {
+        return "admin/welcome";
+    }
+
+    /**
+     * 管理员登录页
+     * @return
+     */
+    @GetMapping("/admin/login")
+    public String adminLoginPage() {
+        return "admin/login";
+    }
+}
+
