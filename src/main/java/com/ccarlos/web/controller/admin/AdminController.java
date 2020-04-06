79b1e74e35d926e9f422292d0f9c637bb4ac5cc7
ccarlos <2238513100@qq.com>
2020-04-06 23:09:39 +0800

    6-3 基于七牛云的图片上传_本地上传(MultipartFile文件上传)

--- src/main/java/com/ccarlos/web/controller/admin/AdminController.java
+++ src/main/java/com/ccarlos/web/controller/admin/AdminController.java
@@ -1,17 +1,16 @@
 package com.ccarlos.web.controller.admin;
 
-import org.springframework.http.HttpStatus;
+import com.ccarlos.base.ApiResponse;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 
 @Controller
 public class AdminController {
@@ -19,6 +18,7 @@ public class AdminController {
 
     /**
      * 后台管理中心
+     *
      * @return
      */
     @GetMapping("/admin/center")
@@ -28,6 +28,7 @@ public class AdminController {
 
     /**
      * 欢迎页
+     *
      * @return
      */
     @GetMapping("/admin/welcome")
@@ -37,6 +38,7 @@ public class AdminController {
 
     /**
      * 管理员登录页
+     *
      * @return
      */
     @GetMapping("/admin/login")
@@ -46,11 +48,39 @@ public class AdminController {
 
     /**
      * 新增房源功能页
+     *
      * @return
      */
     @GetMapping("admin/add/house")
     public String addHousePage() {
         return "admin/house-add";
+    }
+
+    /**
+     * 上传图片接口
+     *
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+
+        String fileName = file.getOriginalFilename();
+        File target = new File
+                ("F:\\fileserver\\"
+                        + fileName);
+
+        try {
+            file.transferTo(target);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+        return ApiResponse.ofSuccess(null);
     }
 }
 
