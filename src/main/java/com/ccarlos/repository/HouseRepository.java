b7e98a61e62360f429ed84edc97344122d3f3c16
ccarlos <2238513100@qq.com>
2020-04-10 21:32:24 +0800

    6-11 房源浏览功能实现_多维度排序

--- src/main/java/com/ccarlos/repository/HouseRepository.java
+++ src/main/java/com/ccarlos/repository/HouseRepository.java
@@ -1,7 +1,9 @@
 package com.ccarlos.repository;
 
 import com.ccarlos.entity.House;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface HouseRepository extends PagingAndSortingRepository<House, Long> {
+public interface HouseRepository extends PagingAndSortingRepository<House, Long>
+        , JpaSpecificationExecutor<House> {
 }
