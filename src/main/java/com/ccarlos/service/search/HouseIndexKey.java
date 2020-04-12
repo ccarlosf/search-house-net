56530706dc0f4be4664d5af2f5ad885a1a429528
ccarlos <2238513100@qq.com>
2020-04-12 11:18:56 +0800

    8-4 索引结构设计（下）

---
+++ src/main/java/com/ccarlos/service/search/HouseIndexKey.java
@@ -0,0 +1,11 @@
+package com.ccarlos.service.search;
+
+/**
+ * 索引关键词统一定义
+ */
+public class HouseIndexKey {
+
+    public static final String HOUSE_ID = "houseId";
+
+    public static final String TITLE = "title";
+}
