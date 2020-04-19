8805de9e92c1b3fcf59381ce103d49dc3a142282
ccarlos <2238513100@qq.com>
2020-04-19 16:38:54 +0800

    10-3 免注册登录_阿里云实现

--- src/main/java/com/ccarlos/web/controller/HomeController.java
+++ src/main/java/com/ccarlos/web/controller/HomeController.java
@@ -1,17 +1,26 @@
 package com.ccarlos.web.controller;
 
 import com.ccarlos.base.ApiResponse;
+import com.ccarlos.base.LoginUserUtil;
+import com.ccarlos.service.ISmsService;
+import com.ccarlos.service.ServiceResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping(value = {"/","/index"})
+    @Autowired
+    private ISmsService smsService;
+
+    @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
-        model.addAttribute("name","Hello World");
+        model.addAttribute("name", "Hello World");
         return "index";
     }
 
@@ -39,5 +48,21 @@ public class HomeController {
     @GetMapping("/logout/page")
     public String logoutPage() {
         return "logout";
+    }
+
+    @GetMapping(value = "sms/code")
+    @ResponseBody
+    public ApiResponse smsCode(@RequestParam("telephone") String telephone) {
+        if (!LoginUserUtil.checkTelephone(telephone)) {
+            return ApiResponse.ofMessage(HttpStatus.BAD_REQUEST.value(),
+                    "请输入正确的手机号");
+        }
+        ServiceResult<String> result = smsService.sendSms(telephone);
+        if (result.isSuccess()) {
+            return ApiResponse.ofSuccess("");
+        } else {
+            return ApiResponse.ofMessage(HttpStatus.BAD_REQUEST.value(), result.getMessage());
+        }
+
     }
 }
